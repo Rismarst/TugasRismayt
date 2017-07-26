ab4fcb06de685d21519b09359581ff473909c50b
Hendriansyh <151610207@smkassalaambandung.sch.id>
2017-07-26 16:33:40 +0700

    Tugas Rumah

---
+++ resources/views/tugasdirumah.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tugas Di Rumah</title>
+</head>
+<body>
+
+	Ini Tugas Rumah
+
+</body>
+</html>
---
+++ resources/views/tugasdisekolah.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tugas Di Sekolah</title>
+</head>
+<body>
+
+	Ini Tugas Sekolah
+
+</body>
+</html>
